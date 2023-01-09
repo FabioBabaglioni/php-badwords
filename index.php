b797d8b3909179e6e9970f8992eb6ede278c001b
FabioBabaglioni <fabiobabaglioni@gmail.com>
2023-01-09 15:36:48 +0100

    Aggiungo conta caratteri

--- index.php
+++ index.php
@@ -10,11 +10,23 @@
 
 <body>
   <div>
+
+    <!-- tag per scrivere php-->
     <?php
+    // creo la variabile con il testo 
     $testo = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam ullam, ut doloribus esse enim voluptates, ipsam nihil veniam, accusamus laboriosam laudantium molestias error. Nam et similique, magnam reiciendis ad modi.";
 
-    echo $testo
-      ?>
+    // stampo il testo 
+    echo $testo;
+    ?>
+  </div>
+
+  <div>
+
+    <?php
+    // stampo la lunghezza in caratteri della variabile testo
+    echo strlen($testo);
+    ?>
   </div>
 
 
