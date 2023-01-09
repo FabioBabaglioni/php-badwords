4ac0f544b99e595deade3185f3c64670a34a3722
FabioBabaglioni <fabiobabaglioni@gmail.com>
2023-01-09 16:39:58 +0100

    Esercizio Completato

--- index.php
+++ index.php
@@ -13,34 +13,34 @@
 
     <!-- tag per scrivere php-->
     <?php
-    // creo la variabile con il testo 
-    $testo = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam ullam, ut doloribus esse enim voluptates, ipsam nihil veniam, accusamus laboriosam laudantium molestias error. Nam et similique, magnam reiciendis ad modi sit.";
+      // creo la variabile con il testo 
+      $testo = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam ullam, ut doloribus esse enim voluptates, ipsam nihil veniam, accusamus laboriosam laudantium molestias error. Nam et similique, magnam reiciendis ad modi sit.";
 
-    // stampo il testo 
-    echo $testo;
+      // stampo il testo 
+      echo $testo;
     ?>
   </div>
 
   <div>
 
     <?php
-    // stampo la lunghezza in caratteri della variabile testo
-    echo strlen($testo);
+      // stampo la lunghezza in caratteri della variabile testo
+      echo strlen($testo);
     ?>
   </div>
 
   <div>
     <?php
-    // sostituice la parola inserita dall'utente con gli asterischi
-    echo str_replace(htmlspecialchars($_GET["badword"]), "***", $testo);
-    echo strlen($testo)
-      ?>
+      // sostituice la parola inserita dall'utente con gli asterischi
+      $testoCensurato = str_replace(htmlspecialchars($_GET["badword"]), "***", $testo);
+      echo $testoCensurato;
+    ?>
   </div>
 
   <div>
     <?php
-    // ricontare i caratteri 
-    echo strlen($testo);
+      // ricontare i caratteri 
+      echo strlen($testoCensurato);
     ?>
   </div>
 
